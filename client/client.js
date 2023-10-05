9cb7006a26871722a1e6823de397c34157f0432a
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-10-05 15:44:28 +0200

    setup basic functionality for RESOLVER.

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@ import {supabase} from "./supabaseClient.js";
 export async function connectorObjects() {
     const {data, error} = await supabase
         .from('dmg_objects_LDES')
-        .select('objectNumber, RESOLVES_TO, PURI, iiif_manifest')
+        .select('objectNumber, RESOLVES_TO, PURI, iiif_manifest, LDES_raw')
     return data;
 }
 
@@ -17,5 +17,12 @@ export async function writePURI(_on, PURI) {
     const{data, error} = await supabase
         .from('dmg_objects_LDES')
         .update({'PURI': PURI})
+        .eq("objectNumber", _on)
+}
+
+export async function writeSTATUS(_on, STATUS) {
+    const {data, error} = await supabase
+        .from('dmg_objects_LDES')
+        .update({'STATUS':STATUS})
         .eq("objectNumber", _on)
 }
