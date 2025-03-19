49fc194fc0e872b7a4da4137703f546444bb4b79
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-03-19 10:57:55 +0100

    Refactor async workflows and improve null safety.
    Optimized multiple functions by adding `await` statements to ensure proper handling of asynchronous operations. Enhanced null and error checks in several modules to prevent runtime issues, and refined logging for better debugging clarity. Simplified duplicate detection in `prunePrivate.js` and added descriptive logging for improved traceability.

--- resolver-full.js
+++ resolver-full.js
@@ -2,11 +2,11 @@ import {monitorHealthUpstream} from "./utils/tests.js";
 import {prunePrivate} from "./utils/prunePrivate.js";
 import {populateIIIF, produceURIs} from "./utils/produceURIs.js";
 
-function resolve() {
-    populateIIIF();
-    produceURIs();
-    prunePrivate();
-    monitorHealthUpstream('ALL')
+async function resolve() {
+    await populateIIIF();
+    await produceURIs();
+    await prunePrivate();
+    await monitorHealthUpstream('ALL')
 }
 
 resolve();
--- utils/prunePrivate.js
+++ utils/prunePrivate.js
@@ -4,26 +4,30 @@ export async function prunePrivate() {
     let streamPrivate = await connectorPrivateStream();
     let streamPublic = await connectorObjects();
 
-    let totalPrivate = streamPrivate.length;
-    let totalPublic = streamPublic.length;
+    if(streamPublic && streamPrivate)  {
+        let totalPrivate = streamPrivate.length;
+        let totalPublic = streamPublic.length;
 
-    for(let i = 0; i < streamPublic.length; i++) {
-        //console.log(streamPublic[i]["objectNumber"])
-        // check if this number is also in the private stream
-        let objectNumber = streamPublic[i]["objectNumber"]
-        if (objectNumber in streamPrivate){
-            console.log("duplicate")
-            writeStatusPrivate(objectNumber, true)
+        for(let i = 0; i < streamPublic.length; i++) {
+            //console.log(streamPublic[i]["objectNumber"])
+            // check if this number is also in the private stream
+            let objectNumber = streamPublic[i]["objectNumber"]
+            if (objectNumber in streamPrivate){
+                console.log(`duplicate record: ${objectNumber}`)
+                writeStatusPrivate(objectNumber, true)
+            }
+            // if not continue
+            // if yes set duplicate to TRUE.
         }
-        // if not continue
-        // if yes set duplicate to TRUE.
+
+        console.log(`total private records: ${totalPrivate}`)
+        console.log(`total public records: ${totalPublic}`)
+
     }
 
 
     //console.log(streamPublic)
 
-    console.log(totalPrivate)
-    console.log(totalPublic)
 }
 
 
